0479f204769edce4bcdfefbb5a463c992ee1acb6
Den <dantist3520@mail.ru>
2022-04-20 20:37:21 +0300

    Первое задание, сделать ДЗ!

--- Занятие1/Практические_задания/task4_2/main.py
+++ Занятие1/Практические_задания/task4_2/main.py
@@ -3,10 +3,10 @@ from itertools import count
 
 def task():
     iterator_numbers = count(1, 1)
-    numbers = [i ** 2 for i in iterator_numbers if i % 2 == 0]  # TODO заменить на map и filter
+    numbers = list(map(lambda num: num**2, filter(lambda num: num % 2 == 0, iterator_numbers)))  # TODO заменить на map и filter
 
-    for num in numbers:  # TODO напечатать первые 10 чисел
-        print(num)  # TODO с помощью next получать следующий элемент от итератора
+    for num in range(10):  # TODO напечатать первые 10 чисел
+        print(next(iterator_numbers))  # TODO с помощью next получать следующий элемент от итератора
 
 
 if __name__ == "__main__":
